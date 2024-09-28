fc2048fcbca816a207296a4fc61f8c76434f24e1
Grace <gracedes@bu.edu>
2024-09-28 15:26:18 -0400

    simplified kmeans implementation

--- kmeans.py
+++ kmeans.py
@@ -1,136 +1,21 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+import matplotlib.cm as cm
 
-blobs = pd.read_csv('kmeans_blobs.csv')
-colnames = list(blobs.columns[1:-1])
-blobs.head()
+cmap = 'Set3'
 
+"""
+kmeans steps:
+1. define number of clusters
+2. initialize centroid to each cluster
+3. calculate distance between each point and centroid
+4. assign each point to the nearest centroid
+5. update centroid location
+6. repeat steps 3 to 5 until convergence
+"""
 
-
-customcmap = ListedColormap(["crimson", "mediumblue", "darkmagenta"])
-
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.scatter(x=blobs['x'], y=blobs['y'], s=150,
-            c=blobs['cluster'].astype('category'), 
-            cmap = customcmap)
-ax.set_xlabel(r'x', fontsize=14)
-ax.set_ylabel(r'y', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.show()
-
-
-"""steps 1 and 2: initialize centroids and assign clusters"""
-
-def initiate_centroids(k, dset):
-    '''
-    Select k data points as centroids
-    k: number of centroids
-    dset: pandas dataframe
-    '''
-    centroids = dset.sample(k)
-    return centroids
-
-np.random.seed(42)
-k=3
-df = blobs[['x','y']]
-centroids = initiate_centroids(k, df)
-centroids
-
-
-"""step 3: calculate distance"""
-
-def rsserr(a,b):
-    '''
-    Calculate the root of sum of squared errors. 
-    a and b are numpy arrays
-    '''
-    return np.square(np.sum((a-b)**2)) 
-
-
-for i, centroid in enumerate(range(centroids.shape[0])):
-    err = rsserr(centroids.iloc[centroid,:], df.iloc[36,:])
-    print('Error for centroid {0}: {1:.2f}'.format(i, err))
-
-
-"""step 4: assign centroids"""
-
-def centroid_assignation(dset, centroids):
-    '''
-    Given a dataframe `dset` and a set of `centroids`, we assign each
-    data point in `dset` to a centroid. 
-    - dset - pandas dataframe with observations
-    - centroids - pa das dataframe with centroids
-    '''
-    k = centroids.shape[0]
-    n = dset.shape[0]
-    assignation = []
-    assign_errors = []
-
-    for obs in range(n):
-        # Estimate error
-        all_errors = np.array([])
-        for centroid in range(k):
-            err = rsserr(centroids.iloc[centroid, :], dset.iloc[obs,:])
-            all_errors = np.append(all_errors, err)
-
-        # Get the nearest centroid and the error
-        nearest_centroid =  np.where(all_errors==np.amin(all_errors))[0].tolist()[0]
-        nearest_centroid_error = np.amin(all_errors)
-
-        # Add values to corresponding lists
-        assignation.append(nearest_centroid)
-        assign_errors.append(nearest_centroid_error)
-
-    return assignation, assign_errors
-
-
-df['centroid'], df['error'] = centroid_assignation(df, centroids)
-df.head()
-
-
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.scatter(df.iloc[:,0], df.iloc[:,1],  marker = 'o', 
-            c=df['centroid'].astype('category'), 
-            cmap = customcmap, s=80, alpha=0.5)
-plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
-            marker = 's', s=200, c=[0, 1, 2], 
-            cmap = customcmap)
-ax.set_xlabel(r'x', fontsize=14)
-ax.set_ylabel(r'y', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.show()
-
-
-print("The total error is {0:.2f}".format(df['error'].sum()))
-
-
-"""step 5: update centroid location"""
-
-centroids = df.groupby('centroid').agg('mean').loc[:, colnames].reset_index(drop = True)
-centroids
-
-
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.scatter(df.iloc[:,0], df.iloc[:,1],  marker = 'o', 
-            c=df['centroid'].astype('category'), 
-            cmap = customcmap, s=80, alpha=0.5)
-plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
-            marker = 's', s=200,
-            c=[0, 1, 2], cmap = customcmap)
-ax.set_xlabel(r'x', fontsize=14)
-ax.set_ylabel(r'y', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.show()
-
-
-"""step 6: repeat steps 3 to 5"""
-
-def kmeans(dset, k=2, tol=1e-4):
+def kmeans(dset, k=3, tol=1e-4):
     '''
     K-means implementationd for a 
     `dset`:  DataFrame with observations
@@ -150,7 +35,7 @@ def kmeans(dset, k=2, tol=1e-4):
 
     while(goahead):
         # Step 3 and 4 - Assign centroids and calculate error
-        working_dset['centroid'], j_err = centroid_assignation(working_dset, centroids) 
+        working_dset['centroid'], j_err = centroid_assignment(working_dset, centroids) 
         err.append(sum(j_err))
         
         # Step 5 - Update centroid position
@@ -163,42 +48,70 @@ def kmeans(dset, k=2, tol=1e-4):
                 goahead = False
         j+=1
 
-    working_dset['centroid'], j_err = centroid_assignation(working_dset, centroids)
+    working_dset['centroid'], j_err = centroid_assignment(working_dset, centroids)
     centroids = working_dset.groupby('centroid').agg('mean').reset_index(drop = True)
+    gen_plot(working_dset, centroids)
     return working_dset['centroid'], j_err, centroids
 
+def initiate_centroids(k, dset):
+    '''
+    Select k data points as centroids
+    k: number of centroids
+    dset: pandas dataframe
+    '''
+    centroids = dset.sample(k)
+    return centroids
 
-np.random.seed(42)
-df['centroid'], df['error'], centroids =  kmeans(df[['x','y']], 3)
-df.head()
+def calc_dist(a,b):
+    '''
+    Calculate the root of sum of squared errors. 
+    a and b are numpy arrays
+    '''
+    return np.square(np.sum((a-b)**2)) 
 
+def centroid_assignment(dset, centroids):
+    '''
+    Given a dataframe `dset` and a set of `centroids`, we assign each
+    data point in `dset` to a centroid. 
+    - dset - pandas dataframe with observations
+    - centroids - pa das dataframe with centroids
+    '''
+    k = centroids.shape[0]
+    n = dset.shape[0]
+    assignation = []
+    assign_errors = []
 
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.scatter(df.iloc[:,0], df.iloc[:,1],  marker = 'o', 
-            c=df['centroid'].astype('category'), 
-            cmap = customcmap, s=80, alpha=0.5)
-plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
-            marker = 's', s=200, c=[0, 1, 2], 
-            cmap = customcmap)
-ax.set_xlabel(r'x', fontsize=14)
-ax.set_ylabel(r'y', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.show()
+    for obs in range(n):
+        # Estimate error
+        all_errors = np.array([])
+        for centroid in range(k):
+            err = calc_dist(centroids.iloc[centroid, :], dset.iloc[obs,:])
+            all_errors = np.append(all_errors, err)
 
+        # Get the nearest centroid and the error
+        nearest_centroid =  np.where(all_errors==np.amin(all_errors))[0].tolist()[0]
+        nearest_centroid_error = np.amin(all_errors)
 
-err_total = []
-n = 10
+        # Add values to corresponding lists
+        assignation.append(nearest_centroid)
+        assign_errors.append(nearest_centroid_error)
 
-df_elbow = blobs[['x','y']]
+    return assignation, assign_errors
 
-for i in range(n):
-    _, my_errs, _ = kmeans(df_elbow, i+1)
-    err_total.append(sum(my_errs))
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.plot(range(1,n+1), err_total, linewidth=3, marker='o')
-ax.set_xlabel(r'Number of clusters', fontsize=14)
-ax.set_ylabel(r'Total error', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.show()
+def gen_plot(df, centroids):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.scatter(df.iloc[:,0], df.iloc[:,1],  marker = 'o', 
+                c=df['centroid'].astype('category'), 
+                cmap = cmap, s=80, alpha=0.5)
+    plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
+                marker = 's', s=200, c=range(centroids.shape[0]), 
+                cmap = cmap)
+    ax.set_xlabel(r'x', fontsize=14)
+    ax.set_ylabel(r'y', fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.show()
+
+blobs = pd.read_csv('kmeans_blobs.csv')
+colnames = list(blobs.columns[1:-1])
+kmeans(blobs[colnames], k=10)
